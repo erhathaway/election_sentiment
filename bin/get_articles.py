bcfd20fdc3ff1bb931e577907b22807b53908d5e
Ethan Hathaway <erhathaway@gmail.com>
2015-08-25 16:31:36 -0400

    add working scraper for search of nytimes site

--- bin/get_articles.py
+++ bin/get_articles.py
@@ -32,29 +32,71 @@ def find_search(site):
 
 
 def find_articles_by_form(site, search_form_index, search_controler_name, search_term):
-    br.open(site)
-    br.select_form(nr=search_form_index)
-    br.form[search_controler_name]=search_term
-    result = br.submit()
-    print result.read()
-    print result.geturl()
+  br.open(site)
+  br.select_form(nr=search_form_index)
+  br.form[search_controler_name]=search_term
+  result = br.submit()
+  print result.read()
+  print result.geturl()
 
-def find_articles_by_url(search_url, slug):
-  # result = br.open(search_url+slug)
+def find_articles_by_url(search_url1, slug, search_url2):
+  # render html with dryscrape
   session = dryscrape.Session()
-  session.visit(search_url+slug)
+  session.visit(search_url1+slug+search_url2)
   eval_url = smart_str(session.url())
   html = session.body() 
-  # print result.read()
-  # print html
+
+  # create html soup
   soup = BeautifulSoup(html)
-  a= soup.find("div", {"id": "searchResults"})
-  print smart_str(a.prettify())
+  search_results = soup.find("div", {"id": "searchResults"})
+  
+  # parse articles
+  for x in search_results.find_all('li', {'class':"story"}):
+    
+    # url
+    link = x.find('a', href=True)
+    print "Found the URL:", link['href']
+    
+    # headline
+    try:
+      headline = x.find('span', {'class':"printHeadline"})
+      print smart_str(headline.contents[0]) 
+    except Exception as e:
+      print e
+
+    # author
+    try:
+      author =  x.find('span', {'class':"byline"})
+      print smart_str(author.contents[0]) 
+    except Exception as e:
+      print e
+    
+    # date
+    try:
+      date = x.find('span', {'class':"dateline"})
+      print smart_str(date.contents[0]) 
+    except Exception as e:
+      print e
+    
+    # summary 
+    try:
+      summary = x.find('p', {'class':"summary"})
+      info = ""
+      for i in summary.contents:
+        if re.match('<.*>', smart_str(i)) is None:
+          info += smart_str(i).strip() + " "
+      print info
+    except Exception as e:
+      print e
+
+    print " "
 
 site = "http://www.nytimes.com"
-search_url = "http://query.nytimes.com/search/sitesearch/?action=click&contentCollection&region=TopBar&WT.nav=searchWidget&module=SearchSubmit&pgtype=Homepage#/"
-search_term = "texas is awesome"
+search_url1 = "http://query.nytimes.com/search/sitesearch/?action=click&contentCollection&region=TopBar&WT.nav=searchWidget&module=SearchSubmit&pgtype=Homepage#/"
+# search_url2 = "/since1851/allresults/1/allauthors/newest/"
+search_url2 = ""
+search_term = "rand paul"
 # find_search(site)
 # find_articles(site, 0, "search-input","texas is awesome")
-find_articles_by_url(search_url, search_term)
+find_articles_by_url(search_url1, search_term, search_url2)
 
