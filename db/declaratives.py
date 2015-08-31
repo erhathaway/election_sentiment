3cb1f5e6e23d3c51438343eb25b4238ed48bfb1e
Ethan Hathaway <erhathaway@gmail.com>
2015-08-30 23:09:44 -0400

    add get_or_create function to orm

--- db/declaratives.py
+++ db/declaratives.py
@@ -9,6 +9,17 @@ from sqlalchemy import create_engine, UniqueConstraint
 def _get_date():
   return datetime.datetime.now()
 
+def get_or_create(session, model, defaults=None, **kwargs):
+    instance = session.query(model).filter_by(**kwargs).first()
+    if instance:
+        return instance, False
+    else:
+        params = dict((k, v) for k, v in kwargs.iteritems() if not isinstance(v, ClauseElement))
+        params.update(defaults or {})
+        instance = model(**params)
+        session.add(instance)
+        return instance, True
+        
 # get Base object
 Base = declarative_base()
 
